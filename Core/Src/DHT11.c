ddb62c50ec34193cdfb34d39d241e6b7dcb0a9ee
dayeon <dana090825@gmail.com>
2026-01-10 16:38:00 +0900

    feat : DHT11 40-bit data read

--- Core/Src/DHT11.c
+++ Core/Src/DHT11.c
@@ -70,4 +70,23 @@ DHT_data DHT_getData(DHT_type t) {
 	}
 	timeout = 0;
 	//-> while은 데이터 시트 MCU Sends out Start Signal to DHT 보고 이해
+
+	//DHT11의 5바이트(40비트)를 읽어 채우고 체크섬이 맞으면 data에 넣고 반환
+	uint8_t rawData[5] = {0, 0, 0, 0, 0}; //datasheet Communication Process: Serial Interface 참고
+	// 0바이트 : 습도 정수, 1바이트 : 습도 소수(보통 0), 2바이트 : 온도 정수, 3바이트 : 온도 소수(보통 0), 4바이트 : 체크섬(하위 8비트)
+
+	for(uint8_t a = 0; a < 5; a++) {
+		for(uint8_t b = 0; b != 255; b--) { //!= 255 를 사용한 이유 : 음수가 없어 언더플로우(... 1->0->255 ...)
+		// 다른 방식도 있지만 파형 그림에 맞추고 비트 시프트 계산이 단순해서 위 방식 사용
+		// -> DHT의 데이터 수신 방식 : high가 얼마나 유지 되었는지 (전압X)
+			uint8_t hT = 0, lT = 0; // highTime, longTime (시간이 아닌 횟수임!)
+
+			// datasheet의 DHT Responses to MCU 참고(두 파형 그림 high 길이 비교)
+			while(!getLine()) lT++;
+			while(getLine()) hT++;
+
+			//hT이 lT보다 더 오래 유지되면 1
+			if(hT > lT) rawData[a] |= (1<<b); //rawData의 a번째 바이트의 b번째 비트를 or연산자로 1로 변경
+		}
+	}
 }
